c27ae974c34b9e40701fc882aea1b8629bb0ae1f
Davi Augusto <davisaugust004@Gmail.com>
2025-07-30 23:56:03 -0300

    feat: Header added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Header } from './components/header/header';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:Header
+    }
+];
