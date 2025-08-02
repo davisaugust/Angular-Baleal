d35812566b777905f9cfaa46a3d13d7236d443d8
Davi Augusto <davisaugust004@Gmail.com>
2025-08-01 23:47:14 -0300

    feat: array html li

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FaIconComponent, RouterModule, CommonModule],
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  faWhatsapp = faWhatsapp;
 
+  menuItems = [
+    { label: 'INÍCIO', link: '/' },
+    { label: 'SERVIÇOS', link: '/' },
+    { label: 'MOTIVOS', link: '/' },
+    { label: 'SOBRE', link: '/' },
+    { label: 'FAQ', link: '/' }
+  ];
 }
